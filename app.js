708889d98bcb81a17b6145e7dd54bbb947a05763
adarsh <adarsh@growpital.com>
2023-06-19 21:41:51 +0530

    added port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 const { MongoClient,ObjectId } = require('mongodb')
 const { GridFSBucket } = require('mongodb')
 // mongodb+srv://adarsh:adarsh@cluster0.o0dnsga.mongodb.net/video-app
-
+const PORT = process.env.PORT  || 4000
 const url = 'mongodb+srv://adarsh:adarsh@cluster0.o0dnsga.mongodb.net/video-app';
 
 // Create a new MongoClient
@@ -68,10 +68,8 @@ app.post('/upload', upload.single('video'), async (req, res) => {
 app.get('/video/:videoId', async (req, res) => {
   try {
     const videoId = req.params.videoId;
-
     // Connect to MongoDB Atlas
     await client.connect();
-
     // Access the database and GridFS bucket
     const database = client.db();
     const bucket = new GridFSBucket(database);
@@ -81,18 +79,14 @@ app.get('/video/:videoId', async (req, res) => {
     if (!videoFile || videoFile.length === 0) {
       return res.status(404).json({ error: 'Video not found' });
     }
-    
     const file = videoFile[0];
     console.log(file);
-
     // Set the response headers for video streaming
     res.setHeader('Content-Type', file?.metadata?.contentType);
     res.setHeader('Content-Length', file?.metadata?.fileSize);
     res.setHeader('Accept-Ranges', 'bytes');
-    console.log('headers');
     // Check if range headers are present
     const range = req.headers.range;
-    console.log(range);
     if (range) {
       const positions = range.replace(/bytes=/, '').split('-');
       const start = parseInt(positions[0], 10);
@@ -102,19 +96,17 @@ app.get('/video/:videoId', async (req, res) => {
       res.setHeader('Content-Range', `bytes ${start}-${end}/${file.length}`);
       res.setHeader('Content-Length', chunkSize);
       res.statusCode = 206;
-
+      console.log(typeof end);
       // Create a read stream with the range of bytes
       const videoStream = bucket.openDownloadStream(new ObjectId(videoId), { start, end });
-
+      // console.log(res);
       // Pipe the video stream in chunks
       videoStream.pipe(res);
     } else {
       // If range headers are not present, stream the entire video
       res.statusCode = 200;
-
       // Create a read stream for the entire video file
       const videoStream = bucket.openDownloadStream(new ObjectId(videoId));
-
       // Pipe the video stream
       videoStream.pipe(res);
     }
@@ -125,6 +117,6 @@ app.get('/video/:videoId', async (req, res) => {
 });
 
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log('Listening on port 4000!')
 });
