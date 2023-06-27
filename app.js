80480848768d205e83d8c44204d2aa28d227109c
adarsh <adarsh@growpital.com>
2023-06-27 23:37:35 +0530

    changed domain

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ let server = app.listen(4000, ()=>{
 
 let io = socket(server,{
   cors:{
-    // origin: 'https://stream-your-video.netlify.app'
-    origin :'http://localhost:3000'
+    origin: 'https://stream-your-video.netlify.app'
+    // origin :'http://localhost:3000'
   }
 })
 
