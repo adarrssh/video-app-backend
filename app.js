e73ae136731473e567880790ad78e38004fd7406
adarsh <adarsh@growpital.com>
2023-06-27 21:47:09 +0530

    removed mongodb path fs etc module

--- app.js
+++ app.js
@@ -1,248 +1,49 @@
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer');
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
-const { MongoClient, ObjectId } = require('mongodb');
-const { GridFSBucket } = require('mongodb');
+const socket = require('socket.io')
 const port = process.env.PORT || 4000;
 const url = 'mongodb+srv://adarsh:adarsh@cluster0.o0dnsga.mongodb.net/video-app';
 const app = express();
 
-const videosFolderPath = path.join(__dirname, 'videos'); // Replace 'videos' with your actual folder name
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Set up multer for handling multipart/form-data (file upload)
-const upload = multer();
-
 app.use(cors());
 
-// Connect to MongoDB Atlas
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log('Connected to MongoDB Atlas cluster');
-  } catch (error) {
-    console.error('Error connecting to MongoDB Atlas cluster:', error);
-    process.exit(1);
+let server = app.listen(4000, ()=>{
+  console.log('server running');
+})
+
+let io = socket(server,{
+  cors:{
+    // origin: 'https://stream-your-video.netlify.app'
+    origin :'http://localhost:3000'
   }
-}
+})
+
+  // // WebSocket connection
+  io.on('connection', (socket) => {
+    console.log(`A user connected ${socket.id}`);
+
+    socket.on('videoForward', (data) => {
+      console.log('forward video ' + data);
+      // Broadcast 'videoPlayed' event to all connected clients except the sender
+      socket.broadcast.emit('videoForward',data);
+    });
+    
+    socket.on('disconnect', (socket) => {
+      console.log('A user disconnected');
+    });
+  });
 
 app.get('/', async (req,res)=>{
   res.status(200).send('home route')
 })
-// Upload video endpoint
-app.post('/upload', upload.single('video'), async (req, res) => {
-  await connectToDatabase();
 
-  try {
-    const db = client.db();
-    const bucket = new GridFSBucket(db);
-
-    const videoStream = req.file.buffer;
-
-    const uploadStream = bucket.openUploadStream('panchayat.mp4', {
-      metadata: {
-        fileSize: req.file.buffer.length,
-        contentType: req.file.mimetype,
-      },
-    });
-
-    uploadStream.end(videoStream);
-
-    uploadStream.on('finish', () => {
-      console.log('Video stored in GridFS');
-      res.send('Video uploaded successfully');
-    });
-
-    uploadStream.on('error', (error) => {
-      console.error('Error storing video in GridFS:', error);
-      res.status(500).send('Error storing video');
-    });
-  } catch (error) {
-    console.error('Error uploading video:', error);
-    res.status(500).send('Error uploading video');
-  }
-});
-
-app.post('/upload-to-localstorage', upload.single('video'), async (req, res) => {
-  try {
-    const videoFile = req.file;
-    if (!videoFile) {
-      res.status(400).send('No video file provided');
-      return;
-    }
-    const originalFileName = videoFile.originalname;
-    console.log(originalFileName);
-    const videosDirectory = path.join(__dirname, 'videos');
-    const videoPath = path.join(videosDirectory, originalFileName);
-    
-    // Create the 'videos' directory if it doesn't exist
-    if (!fs.existsSync(videosDirectory)) {
-      fs.mkdirSync(videosDirectory);
-    }
-    
-    fs.writeFile(videoPath, videoFile.buffer, (error) => {
-      if (error) {
-        console.error('Error storing video:', error);
-        res.status(500).send('Error storing video');
-      } else {
-        console.log('Video stored:', videoPath);
-        res.send('Video uploaded successfully');
-      }
-    });
-  } catch (error) {
-    console.error('Error uploading video:', error);
-    res.status(500).send('Error uploading video');
-  }
-});
-
-// Stream video endpoint
-app.get('/video/:videoId', async (req, res) => {
-  await connectToDatabase();
-
-  try {
-    const videoId = req.params.videoId;
-
-    const db = client.db();
-    const bucket = new GridFSBucket(db);
-
-    const videoFile = await bucket.find({ _id: new ObjectId(videoId) }).toArray();
-
-    if (!videoFile || videoFile.length === 0) {
-      return res.status(404).json({ error: 'Video not found' });
-    }
-
-    const file = videoFile[0];
-
-    res.setHeader('Content-Type', file.metadata.contentType);
-    res.setHeader('Content-Length', file.metadata.fileSize);
-    res.setHeader('Accept-Ranges', 'bytes');
-
-    const range = req.headers.range;
-    console.log(range);
-    if (range) {
-      const positions = range.replace(/bytes=/, '').split('-');
-      const start = parseInt(positions[0], 10);
-      const end = positions[1] ? parseInt(positions[1], 10) : file.length - 1;
-      const chunkSize = end - start + 1;
-
-      res.setHeader('Content-Range', `bytes ${start}-${end}/${file.length}`);
-      res.setHeader('Content-Length', chunkSize);
-      res.statusCode = 206;
-
-      const videoStream = bucket.openDownloadStream(new ObjectId(videoId), { start, end });
-      videoStream.pipe(res);
-    } else {
-      res.statusCode = 200;
-      const videoStream = bucket.openDownloadStream(new ObjectId(videoId));
-      videoStream.pipe(res);
-    }
-  } catch (error) {
-    console.error('Error fetching video:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 
 
-
-app.get('/stream/:videoId', (req, res) => {
-  try {
-    const videoId = req.params.videoId;
-    const videoPath = path.join(__dirname, 'videos', videoId);
-    console.log(videoPath);
-    if (fs.existsSync(videoPath)) {
-      const stat = fs.statSync(videoPath);
-      const fileSize = stat.size;
-      const range = req.headers.range || 'bytes=0-';
-      console.log(range);
-      const positions = range.replace(/bytes=/, '').split('-');
-      const start = parseInt(positions[0], 10);
-      const end = positions[1] ? parseInt(positions[1], 10) : fileSize - 1;
-      const chunkSize = end - start + 1;
-
-      res.writeHead(206, {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunkSize,
-        'Content-Type': 'video/mp4',
-      });
-
-      const videoStream = fs.createReadStream(videoPath, { start, end });
-      videoStream.pipe(res);
-    } else {
-      res.status(404).json({ error: 'Video not found' });
-    }
-  } catch (error) {
-    console.error('Error streaming video:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
-app.get('/videos', (req, res) => {
-  fs.readdir(videosFolderPath, (err, files) => {
-    if (err) {
-      console.error('Error reading videos folder:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-
-    const videoFiles = files.filter((file) => {
-      const filePath = path.join(videosFolderPath, file);
-      const fileStats = fs.statSync(filePath);
-      return fileStats.isFile();
-    });
-
-    res.json({ videos: videoFiles });
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
-// let server = app.listen(4000, ()=>{
-  //   console.log('server running');
-  // })
   
-  // let io = socket(server,{
-  //   cors:{
-  //     origin: 'https://stream-your-video.netlify.app'
-  //     // origin :'http://localhost:3000'
-  //   }
-  // })
-  
-  // // WebSocket connection
-  // io.on('connection', (socket) => {
-  //   console.log(`A user connected ${socket.id}`);
-  //   // Listen for the 'videoPaused' event
-  //   socket.on('videoPaused', () => {
-  //     console.log('Video paused');
-  //     // Broadcast 'videoPaused' event to all connected clients except the sender
-  //     socket.broadcast.emit('videoPaused');
-  //   });
-  
-  //   socket.on('videoPlayed', () => {
-  //     console.log('Video played');
-  //     // Broadcast 'videoPlayed' event to all connected clients except the sender
-  //     socket.broadcast.emit('videoPlayed');
-  //   });
-  
-  //   socket.on('forward video', () => {
-  //     console.log('forward video');
-  //     // Broadcast 'videoPlayed' event to all connected clients except the sender
-  //     socket.broadcast.emit('forward video');
-  //   });
-    
-  //   socket.on('disconnect', () => {
-  //     console.log('A user disconnected');
-  //   });
-  // });
+
   
   
