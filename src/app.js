4c134852f0cb27c963cb9c8fb388e036a6b5e9de
adarrssh <adarsh00502@gmail.com>
2023-08-12 09:49:13 +0530

    refactor:refactor

--- src/app.js
+++ src/app.js
@@ -6,17 +6,17 @@ const authRoutes = require('./routes/auth');
 const streamRoutes = require('./routes/profile');
 const decodeJwt = require('./middleware/decodeJwt')
 const {connectDB} = require('./db/db')
-// connectDB()
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://adarsh:BJtJTeNJSb7KvzmU@cluster0.hi5p8dt.mongodb.net/binge-club?retryWrites=true', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
+connectDB()
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb+srv://adarsh:BJtJTeNJSb7KvzmU@cluster0.hi5p8dt.mongodb.net/binge-club?retryWrites=true', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//     console.log('Connected to MongoDB');
+// });
 
 app.use(cors());
 app.use(bodyParser.json()); // Use body-parser middleware to parse JSON bodies
--- src/db/db.js
+++ src/db/db.js
@@ -1,19 +1,34 @@
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
 
-const url = 'mongodb+srv://adarsh:BJtJTeNJSb7KvzmU@cluster0.hi5p8dt.mongodb.net/?retryWrites=true&w=majority/binge-club-database';
-const mongoClient = new MongoClient(url);
+// const url = 'mongodb+srv://adarsh:BJtJTeNJSb7KvzmU@cluster0.hi5p8dt.mongodb.net/?retryWrites=true&w=majority/binge-club-database';
+// const mongoClient = new MongoClient(url);
 
-const connectDB = async () => {
-  try {
-    await mongoClient.connect();
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await mongoClient.connect();
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//   }
+// };
 
-module.exports = {
-  connectDB,
-  getClient: () => mongoClient,
-};
+// module.exports = {
+//   connectDB,
+//   getClient: () => mongoClient,
+// };
 
+const mongoose = require('mongoose')
+
+const connectDB = () => {
+  mongoose.connect('mongodb+srv://adarsh:BJtJTeNJSb7KvzmU@cluster0.hi5p8dt.mongodb.net/binge-club?retryWrites=true', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+}
+
+module.exports = {connectDB}
